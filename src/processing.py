cab4d962637dfc90b4c155872e0a69b2ff5ebe5a
Дмитрий Кукуйский <kukuiski@gmail.com>
2024-09-02 19:32:13 +0300

    Дописал функции filter_by_state(), sort_by_date()

--- src/processing.py
+++ src/processing.py
@@ -5,11 +5,11 @@ def filter_by_state(list_of_dicts: List[Dict[str, Any]], state: str = "EXECUTED"
     """Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению.
     """
-    pass
+    return [d for d in list_of_dicts if d.get('state') == state]
 
 
-def sort_by_date(list_of_dicts: List[Dict[str, Any]], reverse_order: bool = "True") -> List[Dict[str, Any]]:
+def sort_by_date(list_of_dicts: List[Dict[str, Any]], reverse_order: bool = True) -> List[Dict[str, Any]]:
     """Функция принимает список словарей и возвращает новый список, отсортированный по дате (date)
-    Опциональный ключ задаёт порядок сортировки (по умолчению — убывание)
+    Опциональный ключ задаёт порядок сортировки (по умолчанию — убывание)
     """
-    pass
+    return sorted(list_of_dicts, key=lambda x: x.get('date'), reverse=reverse_order)
